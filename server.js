21b2de8bffd2e4dcdbf8a961191d23fc02c18b22
Eric Dennis <eric.dennis@digisynd.com>
2011-03-10 22:34:36 -0800

    Made it work.

--- server.js
+++ server.js
@@ -1,53 +1,51 @@
 var sys    = require('sys'),
     http   = require('http'),
-    ws     = require("./vendor/ws"),
-    base64 = require('./vendor/base64'),
-    arrays = require('./vendor/arrays');
+    io     = require('socket.io'),
+    static = require('node-static');
 
 // Command line args
-var USERNAME = process.ARGV[2];
-var PASSWORD = process.ARGV[3];
-var KEYWORD  = process.ARGV[4] || "iphone";
+var USERNAME = process.ARGV[2] || process.env.TWITTER_USERNAME;
+var PASSWORD = process.ARGV[3] || process.env.TWITTER_PASSWORD;
+var KEYWORD  = process.ARGV[4] || "disney";
 
 if (!USERNAME || !PASSWORD)
   return sys.puts("Usage: node server.js <twitter_username> <twitter_password> <keyword>");
 
-// Authentication Headers for Twitter
-var auth = base64.encode(USERNAME + ':' + PASSWORD);
-var headers = {
-  'Authorization' : "Basic " + auth,
-  'Host'          : "stream.twitter.com"
-};
-
-var clients = [];
-
-// Connection to Twitter's streaming API
-var twitter = http.createClient(80, "stream.twitter.com");
-var request = twitter.request("GET", "/1/statuses/filter.json?track=" + KEYWORD, headers);
-
-request.addListener('response', function (response) {
-  response.setEncoding("utf8");
-  
-  response.addListener("data", function (chunk) {
-    // Send response to all connected clients
-    
-    clients.each(function(c) {
-      c.write(chunk);
-    });
-  });
+// Set up Twitter
+var twitter = new (require("twitter-node").TwitterNode)({
+  user: USERNAME, 
+  password: PASSWORD, 
+  track: [ KEYWORD ]
 });
-request.end();
+ 
+// Set up static file serving
+var file = new(static.Server)('./public');
 
-// Websocket TCP server
-ws.createServer(function (websocket) {
-  clients.push(websocket);
+// Set up a simple server
+server = http.createServer(function(req, res){
+  req.addListener('end', function() {
+    sys.debug("got HTTP request: " + req);
+    file.serve(req, res);
+  })
+});
+server.listen(8080);
 
-  websocket.addListener("connect", function (resource) {
-    // emitted after handshake
-    sys.debug("connect: " + resource);
-  }).addListener("close", function () {
-    // emitted when server or client closes connection
-    clients.remove(websocket);
+// Set up socket.io
+var socket = io.listen(server);
+
+socket.on('connection', function(client) { 
+  // new client is here!
+  client.on('message', function(resource) {
+    sys.debug("connect: " + resource);  
+  });
+  client.on('disconnect', function() {
     sys.debug("close");
   });
-}).listen(8080);
+}); 
+
+// Set up the tweet stream
+twitter.addListener('tweet', function (tweet) {
+  sys.debug("got a tweet: " + tweet);
+  socket.broadcast(tweet);
+});
+twitter.stream();
